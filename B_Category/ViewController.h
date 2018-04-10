40064cb9a5face89ab572b867a70a266d05f0d93
cocoaroger <wuxuanqiang@isoubu.com>
2018-04-10 22:58:26 +0800

    first commit

---
+++ B_Category/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  B_Category
+//
+//  Created by roger wu on 10/04/2018.
+//  Copyright © 2018 cocoaroger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+use_frameworks!
+inhibit_all_warnings!
+
+def pods
+pod 'CRMediator'
+end
+
+target 'B_Category' do
+    pods
+end
